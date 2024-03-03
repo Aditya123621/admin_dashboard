7417f6072abd925c750fc0e7d000ea91c29bc091
Aditya123621 <adityarox621@example.com>
2024-03-03 18:55:41 +0530

    Confirming changes

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
+  // Adding this line to check to confirm that this branch will be merged with the main
+  console.log("first")
+  return <div>Products</div>;
+};
 
-export default Products
+export default Products;
