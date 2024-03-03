f7e8406b7f794db1e26d49834ea4944b77a85b22
Aditya123621 <adityarox621@example.com>
2024-03-03 17:46:45 +0530

    Added the sidebar, navbar and route

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Dashboard from "./layout";
+
+const DashboardPage = () => {
+  return <div>This will be the Dashboard Page. Will be build</div>;
+};
+
+export default DashboardPage;
---
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -0,0 +1,22 @@
+"use client";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { IoSearch } from "react-icons/io5";
+import styles from "../navbar/navbar.module.css";
+
+const Navbar = () => {
+  const pathname = usePathname();
+  const getNavBarTitle = (e) => {
+    return e[0].toUpperCase() + e.slice(1);
+  };
+  return (
+    <div className="">
+      <div className="">{getNavBarTitle(pathname.split("/").pop())}</div>
+      <div>
+        <IoSearch />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
